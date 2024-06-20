cc3812eadb9680c068282d1d2dbe19f9094a7d12
Steve <stigzler@msn.com>
2024-06-20 09:05:32 +0100

    updated

---
+++ VisGit/MvvmToolkitError.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisGit
+{
+    internal partial class MvvmToolkitError : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _works = false;
+
+        public MvvmToolkitError()
+        {
+            Works = true;
+        }
+    }
+}
